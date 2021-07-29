023fb1f015e517c0cb83a4385891cb0f4be2d1eb
Zaid Hasan <zaid.hasan@primathon.in>
2021-07-29 20:27:30 +0530

    fix:Provider block moved to root

--- iam/provider.tf
+++ iam/provider.tf
@@ -2,11 +2,3 @@ provider "aws" {
   region  = var.region
   profile = "cryptern"
 }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.50.0"
-    }
-  }
-}
