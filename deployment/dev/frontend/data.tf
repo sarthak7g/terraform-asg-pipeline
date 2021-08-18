ecaaa2580fb17d2441d7dfacdb4a8a2761f21cd8
Zaid Hasan <zaid.hasan@primathon.in>
2021-08-18 16:02:00 +0530

    change: Frontend build stage added

--- deployment/dev/frontend/data.tf
+++ deployment/dev/frontend/data.tf
@@ -18,3 +18,13 @@ data "terraform_remote_state" "cryptern-infra-frontend-autoscaling" {
     profile        = "cryptern"
   }
 }
+data "terraform_remote_state" "cryptern-infra-data" {
+  backend = "s3"
+  config = {
+    bucket         = "tf-remote-state.cryptern.dev"
+    key            = "infrastructure-cryptern.dev.tfstate"
+    dynamodb_table = "tf-remote-state-lock.cryptern.dev"
+    region         = "us-east-1"
+    profile        = "cryptern"
+  }
+}
---
+++ deployment/frontend/variables.tf
@@ -0,0 +1,45 @@
+variable "env" {
+
+}
+variable "region" {
+
+}
+variable "codeDeployRoleArn" {
+
+}
+variable "deploymentConfigName" {
+
+}
+variable "codepipelineRoleArn" {
+
+}
+variable "targetGroup" {
+
+}
+variable "asg" {
+
+}
+variable "part" {
+
+}
+variable "appZip" {
+
+}
+variable "codeBuildRoleArn" {
+
+}
+variable "envVariables" {
+  type = list(string)
+}
+variable "vpc" {
+
+}
+variable "privateSubnet" {
+
+}
+variable "publicSubnet" {
+
+}
+variable "publicSecurityGroupId" {
+
+}
