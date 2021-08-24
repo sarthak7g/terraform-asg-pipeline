44cb8b390937ab308fa4058193e0475f6ba5aeb8
Zaid Hasan <zaid.hasan@primathon.in>
2021-08-25 00:02:18 +0530

    feature: backend deployment added

---
+++ deployment/stg/backend/main.tf
@@ -0,0 +1,12 @@
+module "cryptern-deployment-backend" {
+  source               = "../../backend/"
+  env                  = "stg"
+  region               = "us-east-1"
+  part                 = "backend"
+  codeDeployRoleArn    = data.terraform_remote_state.cryptern-iam-data.outputs.codedeployrole.arn
+  deploymentConfigName = "CodeDeployDefault.OneAtATime"
+  asg                  = data.terraform_remote_state.cryptern-infra-backend-autoscaling.outputs.asg.name
+  targetGroup          = data.terraform_remote_state.cryptern-infra-backend-autoscaling.outputs.targetGroup[0].name
+  codepipelineRoleArn  = data.terraform_remote_state.cryptern-iam-data.outputs.codepipeline-backend-role.arn
+  appZip               = "crypterns-api.zip"
+}
