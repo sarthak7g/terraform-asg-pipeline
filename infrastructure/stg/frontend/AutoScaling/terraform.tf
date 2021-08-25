ae2cc191ac96e1fc3807fad262c54814817a4d09
Zaid Hasan <zaid.hasan@primathon.in>
2021-08-25 13:44:39 +0530

    feature: ASG added for frontend stg

---
+++ infrastructure/stg/frontend/AutoScaling/terraform.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = "~>1.0.2"
+  backend "s3" {
+    bucket         = "tf-remote-state.cryptern.stg"
+    key            = "infrastructure-cryptern-autoscaling-frontend.stg.tfstate"
+    dynamodb_table = "tf-remote-state-lock.cryptern.stg"
+    region         = "us-east-1"
+    profile        = "cryptern"
+  }
+}
