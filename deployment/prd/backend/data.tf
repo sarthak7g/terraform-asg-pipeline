df24d1d9a90560268f403ff2912621c4b1333fe7
Zaid Hasan <zaid.hasan@primathon.in>
2021-10-06 22:59:44 +0530

    Deployment Infra added Backend

---
+++ deployment/prd/backend/data.tf
@@ -0,0 +1,20 @@
+data "terraform_remote_state" "cryptern-iam-data" {
+  backend = "s3"
+  config = {
+    bucket         = "tf-remote-state.cryptern.prd"
+    key            = "iam-cryptern.prd.tfstate"
+    dynamodb_table = "tf-remote-state-lock.cryptern.prd"
+    region         = "us-east-1"
+    profile        = "cryptern"
+  }
+}
+data "terraform_remote_state" "cryptern-infra-backend-autoscaling" {
+  backend = "s3"
+  config = {
+    bucket         = "tf-remote-state.cryptern.prd"
+    key            = "infrastructure-cryptern-autoscaling-backend.prd.tfstate"
+    dynamodb_table = "tf-remote-state-lock.cryptern.prd"
+    region         = "us-east-1"
+    profile        = "cryptern"
+  }
+}
