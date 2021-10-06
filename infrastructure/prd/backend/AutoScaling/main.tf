3e3a0c4c8c2f714c38c63903d1b94f28599d5a56
Zaid Hasan <zaid.hasan@primathon.in>
2021-10-06 22:54:18 +0530

    Auto scaling infra Added for backend

---
+++ infrastructure/prd/backend/AutoScaling/main.tf
@@ -0,0 +1,32 @@
+module "cryptern-infra-backend-autoscaling" {
+  source                 = "../../../backend/AutoScaling"
+  env                    = "prd"
+  region                 = "us-east-1"
+  referenceInstanceAmi   = data.terraform_remote_state.cryptern-static-backend-data.outputs.reference-instance-backend.id
+  privateSecurityGroupId = [data.terraform_remote_state.cryptern-infra-data.outputs.private-sg.id]
+  instanceType           = "t3a.medium"
+  instanceProfileName    = data.terraform_remote_state.cryptern-iam-data.outputs.instance-profile.name
+  maxSize                = 10
+  minSize                = 1
+  healthCheckGracePeriod = 300
+  healthCheckType        = "ELB"
+  privateSubnet          = [for subnet in data.terraform_remote_state.cryptern-infra-data.outputs.private-subnet : subnet.id]
+  publicSubnet           = [for subnet in data.terraform_remote_state.cryptern-infra-data.outputs.public-subnet : subnet.id]
+  vpc                    = data.terraform_remote_state.cryptern-infra-data.outputs.vpc
+  protocolType           = "HTTP"
+  backendPorts           = [80]
+  appNames               = ["cryptern"]
+  loadBalancerPort       = 80
+  hostNames              = ["api.crypterns.com", "chat.crypterns.com"]
+  fixedResponseType      = "application/json"
+  fixedResponseMessage   = "Success"
+  fixedResponseStatus    = 200
+  publicSecurityGroupId  = [data.terraform_remote_state.cryptern-infra-data.outputs.public-sg.id]
+  healthCheckPath        = ["/"]
+  healthCheckPorts       = [80]
+  policyType             = "TargetTrackingScaling"
+  predefinedMetricType   = "ASGAverageCPUUtilization"
+  targetValue            = 80.0
+  disableScaleIn         = false
+  stickinessTime         = 3600
+}
