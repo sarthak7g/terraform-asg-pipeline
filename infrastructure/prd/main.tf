70e0c6c7fab5fb2d55252376a3b43c7164259928
Zaid Hasan <zaid.hasan@primathon.in>
2021-10-19 15:55:04 +0530

    FIX: Ip address should start with 3 for prod

--- infrastructure/prd/main.tf
+++ infrastructure/prd/main.tf
@@ -3,9 +3,9 @@ module "cryptern-infra" {
   env                    = "prd"
   region                 = "us-east-1"
   availabilityZones      = ["us-east-1a", "us-east-1b"]
-  privateSubnets         = ["20.0.0.0/24", "20.0.2.0/24"]
-  publicSubnets          = ["20.0.1.0/24", "20.0.3.0/24"]
+  privateSubnets         = ["30.0.0.0/24", "30.0.2.0/24"]
+  publicSubnets          = ["30.0.1.0/24", "30.0.3.0/24"]
   jumpServerInstanceType = "t2.micro"
   publicKey              = file("/home/zaid/.ssh/id_rsa.pub")
-  vpcCidrBlock           = "20.0.0.0/16"
+  vpcCidrBlock           = "30.0.0.0/16"
 }
